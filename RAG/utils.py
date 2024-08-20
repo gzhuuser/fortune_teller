757667dfa4486f8d77ee37922f4f435bd201d87c
Sxb <2368203939@qq.com>
2024-08-20 23:03:20 +0800

    修改了后端部分接口
    修改了后端部分接口

--- RAG/utils.py
+++ RAG/utils.py
@@ -1,20 +1,11 @@
 from typing import Dict, Any
 from modelscope import snapshot_download
-from Knowledge import KnowledgeBase
-
-# 下载模型并得到模型在本地存储的路径
-model_dir = snapshot_download("AI-ModelScope/bge-large-zh-v1.5", revision="master")
-instruction = "为这个句子生成表示以用于检索相关文章"
-
-# 初始化并加载知识库
-knowledge_base = KnowledgeBase(model_dir, instruction)
-knowledge_base.load_knowledge("./RAG/data/knowledge_embeddings_with_indices.pkl")
-
-# ------------------上面这一部分必须要在使用下面的函数前预先加载好
-# knowledge_embeddings_with_indices.pkl已经是处理好的向量数据了,到时候直接使用load_knowledge加载即可
+from RAG.Knowledge import KnowledgeBase
 
 
-def rag_matching(input_data: Dict[str, str], knowledge_base: Any, topk: int = 1) -> str:
+def rag_matching(
+    input_data: Dict[str, str], knowledge_base: Any, feature, topk: int = 1
+) -> str:
     """
     根据输入的掌纹特征，通过知识库匹配相关的知识。
 
@@ -50,17 +41,3 @@ def rag_matching(input_data: Dict[str, str], knowledge_base: Any, topk: int = 1)
             result_str += "\n"
 
     return result_str.strip()
-
-
-# 使用示例:
-if __name__ == "__main__":
-    input_data = {
-        "感情线": "较长,弯曲,较深,无分叉",
-        "生命线": "中等长度,弧形,深",
-        "智慧线": "较长,直线,浅,末端分叉",
-        "婚姻线": "两条,一条较长,一条较短,弯曲",
-        "事业线": "较长,直线,深",
-    }
-
-    result = rag_matching(input_data, knowledge_base)
-    print(result)
